c5d93fde46cb1b52e917300f1feee383ecc03649
Göksel Köksal <gokselkoksal@gmail.com>
2019-01-04 17:38:58 +0000

    Initial Commit

---
+++ Rasat/Rasat.h
@@ -0,0 +1,19 @@
+//
+//  Rasat.h
+//  Rasat
+//
+//  Created by Göksel Köksal on 4.01.2019.
+//  Copyright © 2019 GK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Rasat.
+FOUNDATION_EXPORT double RasatVersionNumber;
+
+//! Project version string for Rasat.
+FOUNDATION_EXPORT const unsigned char RasatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rasat/PublicHeader.h>
+
+
